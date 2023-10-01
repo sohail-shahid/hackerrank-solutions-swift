507a8a1e7b2e85eff91a0795facf4d9adbf42bcd
Sohail Khan <sohail0059@gmail.com>
2023-10-01 14:24:00 +0500

    Turing Coding challenge

---
+++ HackerRankSolutionsSwift.playground/Sources/TuringCodingChalenge/TuringCodingChalenge.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+func simpleArraySum(ar: [Int]) -> Int {
+    // Write your code here
+    let sum = ar.reduce(0) { result, element in
+        result + element
+    }
+    return sum
+}
+
+func compareTriplets(a: [Int], b: [Int]) -> [Int] {
+    // already done
+}
+
+func aVeryBigSum(ar: [Int]) -> Int64 {
+    // already done
+}
+
+func extraLongFactorials(n: Int) -> Void {
+    // already done
+}
